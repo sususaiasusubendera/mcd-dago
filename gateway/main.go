63e862723f94f5c117f598ed39248fb029e68ef1
sususaiasusubendera <oplessnewbee@gmail.com>
2025-11-20 20:52:35 +0700

    feat: (gateway) setup gateway

---
+++ gateway/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const (
+	httpAddr = ":8080"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	handler := NewHandler()
+	handler.registerRoutes(mux)
+
+	log.Printf("Starting HTTP server at %s", httpAddr)
+
+	if err := http.ListenAndServe(httpAddr, mux); err != nil {
+		log.Fatal("Failed to star HTTP server")
+	}
+}
